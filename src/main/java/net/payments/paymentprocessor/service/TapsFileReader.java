4efc2c0854f7aafb10e847c93b459317a4c76b69
Steven Manikiam <sman@manikiam.net>
2022-01-31 12:16:07 +1100

    Date fime file format string.

--- src/main/java/net/payments/paymentprocessor/service/TapsFileReader.java
+++ src/main/java/net/payments/paymentprocessor/service/TapsFileReader.java
@@ -1,15 +1,31 @@
 package net.payments.paymentprocessor.service;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import lombok.Getter;
 import lombok.Setter;
+import net.payments.paymentprocessor.domain.TapType;
 import net.payments.paymentprocessor.domain.TapsInput;
 
+import java.io.FileReader;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Setter
 public class TapsFileReader {
+
+    public static String FORMAT = "dd-MM-yyyy HH:mm:ss";
 
     String filename;
     private List<TapsInput> tapsInputList;
