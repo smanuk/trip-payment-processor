954983732348ea91b057df5a44434f3333b77a82
Steven Manikiam <sman@manikiam.net>
2022-01-31 11:38:03 +1100

    Added lombok builder to input data.

--- src/main/java/net/payments/paymentprocessor/domain/TapsInput.java
+++ src/main/java/net/payments/paymentprocessor/domain/TapsInput.java
@@ -1,5 +1,6 @@
 package net.payments.paymentprocessor.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.time.ZonedDateTime;
 
 @Getter
 @Setter
+@Builder
 public class TapsInput {
 
     String id;
